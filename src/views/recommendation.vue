65918d9cb12cb356bf819c7c76fcdbcd0ef1e0df
Peng Chong <1551127928@qq.com>
2019-03-01 02:08:10 +0800

    Add Recommendation

--- src/views/recommendation.vue
+++ src/views/recommendation.vue
@@ -1,17 +1,61 @@
 <template>
   <div>
     <div class="sidebar"><sidebar /></div>
+    <div class="menus">
+      <div class="d-flex justify-between align-center mb-3">
+        <v-btn @click="all">Display All</v-btn>
+        <v-btn @click="none">Hide ALL</v-btn>
+      </div>
 
+      <v-expansion-panel v-model="panel" expand>
+        <v-expansion-panel-content v-for="item in items" :key="item.title">
+          <div slot="header">{{ item.title }}</div>
+          <v-card>
+            <v-card-text>{{ item.text }}</v-card-text>
+            {{ item.component }}
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </div>
   </div>
 </template>
 
 <script>
 import sidebar from "@/components/Sidebar.vue";
+import list from "@/components/PeList.vue";
 
 export default {
   name: "recommendation",
   components: {
     sidebar
+  },
+  data() {
+    return {
+      panel: [],
+      items: [
+        {
+          title: "Popular Program Electives",
+          text: "Top Ten Chosen PE modules",
+          components: list
+        },
+        {
+          title: "Common Minors Taken",
+          text: "Top Three Minors Selected"
+        },
+        {
+          title: "Most Sorted Hard Skills",
+          text: "Technical Skills Industry Rank"
+        }
+      ]
+    };
+  },
+  methods: {
+    all() {
+      this.panel = [...Array(this.items.length).keys()].map(_ => true);
+    },
+    none() {
+      this.panel = [];
+    }
   }
 };
 </script>
@@ -28,14 +72,9 @@ export default {
   width: 20%;
   float: left;
 }
-.dropdown {
-  width: 20%;
+.menus {
+  width: 80%;
   float: left;
-}
-.studyplan .studyplan_blank {
-  width: 50%;
-  float: left;
-
 }
 h1,
 h2 {
