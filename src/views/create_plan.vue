d85917bf27b5a567ede884f430d4bd6bedbb7846
Peng Chong <1551127928@qq.com>
2019-04-16 17:37:55 +0800

    test input table

--- src/views/create_plan.vue
+++ src/views/create_plan.vue
@@ -1,117 +1,20 @@
 <template lang="html">
-<div id="app">
+<div>
  <div class="sidebar"><sidebar /></div>
- <div id="table-wrapper" class="ui container">
-   <h2> Create Your Own Study Plan</h2>
-  <vuetable ref="vuetable"
-    api-url="https://vuetable.ratiw.net/api/users"
-    :fields="fields"
-    :sort-order="sortOrder"
-    :css="css.table"
-    pagination-path=""
-    :per-page="3"
-    @vuetable:pagination-data="onPaginationData"
-    @vuetable:loading="onLoading"        
-    @vuetable:loaded="onLoaded"
-  >
-    <template slot="actions" scope="props">
-      <div class="table-button-container">
-          <button class="btn btn-warning btn-sm" @click="editRow(props.rowData)">
-            <span class="glyphicon glyphicon-pencil"></span> Edit</button>&nbsp;&nbsp;
-          <button class="btn btn-danger btn-sm" @click="deleteRow(props.rowData)">
-            <span class="glyphicon glyphicon-trash"></span> Delete</button>&nbsp;&nbsp;
-      </div>
-      </template>
-    </vuetable>
-    <vuetable-pagination ref="pagination"
-      :css="css.pagination"
-      @vuetable-pagination:change-page="onChangePage"
-    ></vuetable-pagination>
-  </div>
+ <div><table /></div>
 </div>
 </template>
 
 <script>
 import sidebar from "@/components/Sidebar.vue";
-import VueTable, {
-  VuetablePagination,
-  VuetablePaginationInfo
-} from "vuetable-2";
+import table from "@/components/input_table.vue";
 
 export default {
   name: "create_plan",
   data() {
     return {
-      fields: [
-        {
-          name: "time",
-          title:
-            '<span class="orange glyphicon glyphicon-user"></span> Semester',
-          sortField: "time"
-        },
-        "Module 1",
-        "Module 2",
-        "Module 3",
-        "Module 4",
-        "Module 5",
-        "Overload Modules",
-        "__slot:actions"
-      ],
-      sortOrder: [{ field: "time", direction: "asc" }],
-      css: {
-        table: {
-          tableClass: "table table-striped table-bordered table-hovered",
-          loadingClass: "loading",
-          ascendingIcon: "glyphicon glyphicon-chevron-up",
-          descendingIcon: "glyphicon glyphicon-chevron-down",
-          handleIcon: "glyphicon glyphicon-menu-hamburger"
-        },
-        pagination: {
-          infoClass: "pull-left",
-          wrapperClass: "vuetable-pagination pull-right",
-          activeClass: "btn-primary",
-          disabledClass: "disabled",
-          pageClass: "btn btn-border",
-          linkClass: "btn btn-border",
-          icons: {
-            first: "",
-            prev: "",
-            next: "",
-            last: ""
-          }
-        }
-      }
-    };
-  },
-  components: {
-    sidebar,
-    vuetable: VueTable,
-    "vuetable-pagination": VuetablePagination,
-  },
-  methods: {
-    onPaginationData(paginationData) {
-      this.$refs.pagination.setPaginationData(paginationData);
-    },
-    onChangePage(page) {
-      this.$refs.vuetable.changePage(page);
-    },
-    editRow(rowData) {
-      alert("You clicked edit on" + JSON.stringify(rowData));
-    },
-    deleteRow(rowData) {
-      alert("You clicked delete on" + JSON.stringify(rowData));
-    },
-    onLoading() {
-      console.log("loading... show your spinner here");
-    },
-    onLoaded() {
-      console.log("loaded! .. hide your spinner here");
+
     }
-  },
-  computed: {
-    /*httpOptions(){
-    return {headers: {'Authorization': "my-token"}} //table props -> :http-options="httpOptions"
-  },*/
   }
 };
 </script>
@@ -122,20 +25,5 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   width: 20%;
   float: left;
-}
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-.orange.glyphicon {
-  color: orange;
-}
-
-th.sortable {
-  color: #ec971f;
 }
 </style>
