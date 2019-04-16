882abb21b5822b3aebcb0a0d3dc2efd256d38b3a
Peng Chong <1551127928@qq.com>
2019-04-16 17:58:42 +0800

    add input table

---
+++ src/views/create_plan2.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+<div>
+ <div class="sidebar"><sidebar /></div>
+ <div><table /></div>
+</div>
+</template>
+
+<script>
+import sidebar from "@/components/Sidebar.vue";
+
+
+export default {
+  name: "create_plan",
+  data() {
+    return {
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+.sidebar {
+  background-color: #ffffff;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  width: 20%;
+  float: left;
+}
+</style>
