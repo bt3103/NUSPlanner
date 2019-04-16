6abdc9d72b01e275bf32391feb561fe20608a374
Peng Chong <1551127928@qq.com>
2019-04-16 22:12:29 +0800

    fix bug

--- src/views/create_plan.vue
+++ src/views/create_plan.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="sidebar"><sidebar /></div>
+    <div>
     <v-data-table
       :headers="headers"
       :items="modules"
@@ -8,16 +9,16 @@
       <template v-slot:items="props">
         <td>
           <v-edit-dialog
-            :return-value.sync="props.item.name"
+            :return-value.sync="props.item.semester"
             lazy
             @save="save"
             @cancel="cancel"
             @open="open"
             @close="close"
-          > {{ props.item.name }}
+          > {{ props.item.semster }}
             <template v-slot:input>
               <v-text-field
-                v-model="props.item.name"
+                v-model="props.item.semster"
                 :rules="[max25chars]"
                 label="Edit"
                 single-line
@@ -26,149 +27,220 @@
             </template>
           </v-edit-dialog>
         </td>
-        <td class="text-xs-right">{{ props.item.calories }}</td>
-        <td class="text-xs-right">{{ props.item.fat }}</td>
-        <td class="text-xs-right">{{ props.item.carbs }}</td>
-        <td class="text-xs-right">{{ props.item.protein }}</td>
-        <td class="text-xs-right">
+        <td>
           <v-edit-dialog
-            :return-value.sync="props.item.iron"
-            large
+            :return-value.sync="props.item.module1"
             lazy
-            persistent
             @save="save"
             @cancel="cancel"
             @open="open"
             @close="close"
-          >
-            <div>{{ props.item.iron }}</div>
-            <template v-slot:input>
-              <div class="mt-3 title">Update Iron</div>
-            </template>
+          > {{ props.item.module1 }}
             <template v-slot:input>
               <v-text-field
-                v-model="props.item.iron"
+                v-model="props.item.module1"
                 :rules="[max25chars]"
                 label="Edit"
                 single-line
                 counter
-                autofocus
+              ></v-text-field>
+            </template>
+          </v-edit-dialog>
+        </td>
+        <td>
+          <v-edit-dialog
+            :return-value.sync="props.item.module2"
+            lazy
+            @save="save"
+            @cancel="cancel"
+            @open="open"
+            @close="close"
+          > {{ props.item.module2 }}
+            <template v-slot:input>
+              <v-text-field
+                v-model="props.item.module2"
+                :rules="[max25chars]"
+                label="Edit"
+                single-line
+                counter
+              ></v-text-field>
+            </template>
+          </v-edit-dialog>
+        </td>
+        <td>
+          <v-edit-dialog
+            :return-value.sync="props.item.module3"
+            lazy
+            @save="save"
+            @cancel="cancel"
+            @open="open"
+            @close="close"
+          > {{ props.item.module3 }}
+            <template v-slot:input>
+              <v-text-field
+                v-model="props.item.module3"
+                :rules="[max25chars]"
+                label="Edit"
+                single-line
+                counter
+              ></v-text-field>
+            </template>
+          </v-edit-dialog>
+        </td>
+        <td>
+          <v-edit-dialog
+            :return-value.sync="props.item.module4"
+            lazy
+            @save="save"
+            @cancel="cancel"
+            @open="open"
+            @close="close"
+          > {{ props.item.module4 }}
+            <template v-slot:input>
+              <v-text-field
+                v-model="props.item.module4"
+                :rules="[max25chars]"
+                label="Edit"
+                single-line
+                counter
+              ></v-text-field>
+            </template>
+          </v-edit-dialog>
+        </td>
+        <td>
+          <v-edit-dialog
+            :return-value.sync="props.item.module5"
+            lazy
+            @save="save"
+            @cancel="cancel"
+            @open="open"
+            @close="close"
+          > {{ props.item.module5 }}
+            <template v-slot:input>
+              <v-text-field
+                v-model="props.item.module5"
+                :rules="[max25chars]"
+                label="Edit"
+                single-line
+                counter
               ></v-text-field>
             </template>
           </v-edit-dialog>
         </td>
       </template>
     </v-data-table>
+    </div>
 
-    <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
-      {{ snackText }}
-      <v-btn flat @click="snack = false">Close</v-btn>
-    </v-snackbar>
+    <!-- <v-modbar v-model="mod" :timeout="3000" :color="modColor">
+      {{ modText }}
+      <v-btn flat @click="mod = false">Close</v-btn>
+    </v-modbar> -->
   </div>
 </template>
 
 <script>
 import sidebar from "@/components/Sidebar.vue";
 
-
 export default {
   name: "create_plan",
+  components: {
+    sidebar
+  },
   data () {
       return {
-        snack: false,
-        snackColor: '',
-        snackText: '',
+        mod: false,
+        modColor: '',
+        modText: '',
         max25chars: v => v.length <= 25 || 'Input too long!',
         pagination: {},
         headers: [
           {
             text: 'Semester',
             align: 'left',
-            sortable: false,
-            value: 'name'
+            value: 'semester'
           },
-          { text: 'Module 1', value: 'BT1101' },
-          { text: 'Module 2', value: 'CS1010A' },
-          { text: 'Module 3', value: 'GEH1001' },
-          { text: 'Module 4', value: 'IS1103' },
-          { text: 'Module 5', value: 'MKT1003X' },
-          { text: 'Overload Modules', value: ' ' }
+          { text: 'Module 1',sortable: false, value: 'module1' },
+          { text: 'Module 2', sortable: false, value: 'module2' },
+          { text: 'Module 3', sortable: false, value: 'module3' },
+          { text: 'Module 4', sortable: false, value: 'module4' },
+          { text: 'Module 5', sortable: false, value: 'module5' },
+          { text: 'Overload Modules',sortable: false,  value: 'overloadmods' }
         ],
         modules: [
           {
             name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
+            module1: 159,
+            module2: 6.0,
+            module3: 24,
+            module4: 4.0,
+            module5: '1%'
           },
           {
             name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%'
+            module1: 237,
+            module2: 9.0,
+            module3: 37,
+            module4: 4.3,
+            module5: '1%'
           },
           {
             name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%'
+            module1: 262,
+            module2: 16.0,
+            module3: 23,
+            module4: 6.0,
+            module5: '7%'
           },
           {
             name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%'
+            module1: 305,
+            module2: 3.7,
+            module3: 67,
+            module4: 4.3,
+            module5: '8%'
           },
           {
             name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%'
+            module1: 356,
+            module2: 16.0,
+            module3: 49,
+            module4: 3.9,
+            module5: '16%'
           },
           {
             name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%'
+            module1: 375,
+            module2: 0.0,
+            module3: 94,
+            module4: 0.0,
+            module5: '0%'
           },
           {
             name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%'
+            module1: 392,
+            module2: 0.2,
+            module3: 98,
+            module4: 0,
+            module5: '2%'
           }
         ]
       }
     },
     methods: {
       save () {
-        this.snack = true
-        this.snackColor = 'success'
-        this.snackText = 'Data saved'
+        this.mod = true
+        this.modColor = 'success'
+        this.modText = 'Data saved'
       },
       cancel () {
-        this.snack = true
-        this.snackColor = 'error'
-        this.snackText = 'Canceled'
+        this.mod = true
+        this.modColor = 'error'
+        this.modText = 'Canceled'
       },
       open () {
-        this.snack = true
-        this.snackColor = 'info'
-        this.snackText = 'Dialog opened'
+        this.mod = true
+        this.modColor = 'info'
+        this.modText = 'Dialog opened'
       },
       close () {
         console.log('Dialog closed')
